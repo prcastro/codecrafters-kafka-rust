a4a49f800de5a8420bc7b1b7c21fcd1cb070e10e
Paulo Castro <p.oliveira.castro@gmail.com>
2025-09-14 19:59:00 -0300

    Handle APIVersions requests

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,27 @@ fn main() -> io::Result<()> {
 
                 // Logic
                 let error_code: i16 = if api_version != 4 { 35 } else { 0 };
+                let array_length: u8 = 2;
+                let api_key: i16 = 18; // API_VERSIONS
+                let min_supported_version: i16 = 0;
+                let max_supported_version: i16 = 4;
+                let tag_buffer: u8 = 0;
+                let throttle_time: i32 = 0;
 
-                // Write result
-                stream.write_all(&[0, 0, 0, 6])?;
+                // Write message_size
+                stream.write_all(&[0, 0, 0, 14])?;
+
+                // Write header
                 stream.write_all(correlation_id)?;
+
+                // Write body
                 stream.write_all(&error_code.to_be_bytes())?;
+                stream.write_all(&array_length.to_be_bytes())?;
+                stream.write_all(&api_key.to_be_bytes())?;
+                stream.write_all(&min_supported_version.to_be_bytes())?;
+                stream.write_all(&max_supported_version.to_be_bytes())?;
+                stream.write_all(&tag_buffer.to_be_bytes())?;
+                stream.write_all(&throttle_time.to_be_bytes())?;
             }
             Err(e) => {
                 println!("error: {}", e);
