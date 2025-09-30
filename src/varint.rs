0c95f9d7b6b69b90f26300b72f744a9027fe90d9
Paulo Castro <p.oliveira.castro@gmail.com>
2025-09-30 13:43:31 -0300

    Varint file

---
+++ src/varint.rs
@@ -0,0 +1,38 @@
+// Export the Varint trait and its implementation for &[u8]
+
+use bytes::Buf;
+
+pub trait Varint {
+    fn get_signed_varint(self) -> i64;
+}
+
+impl Varint for &[u8] {
+    fn get_signed_varint(mut self) -> i64 {
+        let mut result = 0;
+        let mut shift = 0;
+        loop {
+            let byte = self.get_u8();
+            let value = (byte & 0b0111_1111) as i64;
+            result |= value << shift;
+            shift += 7;
+            if byte & 0b1000_0000 == 0 {
+                break;
+            }
+        }
+        (result >> 1) ^ -(result & 1)
+    }
+}
+
+#[test]
+fn test_decode() {
+    let input: [u8; 2] = [0b1000_0010, 0b0000_0001];
+    let expected_output = 65;
+    assert_eq!(expected_output, input.get_signed_varint());
+}
+
+#[test]
+fn test_decode_minus_one() {
+    let input: [u8; 1] = [0x01];
+    let expected_output = -1;
+    assert_eq!(expected_output, input.get_signed_varint());
+}
